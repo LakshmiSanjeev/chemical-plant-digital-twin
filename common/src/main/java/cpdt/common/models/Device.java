829cba5f8c9b6cb845e7fdd3bde839f221403c70
LakshmiSanjeev <lakshmi.sanjeev2023@vitstudent.ac.in>
2026-06-03 14:45:55 +0530

    Device models

--- common/src/main/java/cpdt/common/models/Device.java
+++ common/src/main/java/cpdt/common/models/Device.java
@@ -3,13 +3,21 @@ package cpdt.common.models;
 import cpdt.common.enums.DeviceStatus;
 import cpdt.common.enums.DeviceType;
 
-public class Device {
+public abstract class Device {
+
     private String deviceId;
     private String name;
-
     private DeviceType type;
-
     private Location location;
-
     private DeviceStatus status;
+
+    protected Device(){}
+
+    protected Device(String deviceId, String name, DeviceType type, Location location, DeviceStatus status){
+        this.deviceId = deviceId;
+        this.name = name;
+        this.type = type;
+        this.location = location;
+        this.status = status;
+    }
 }
--- common/src/main/java/cpdt/common/models/Location.java
+++ common/src/main/java/cpdt/common/models/Location.java
@@ -3,9 +3,16 @@ package cpdt.common.models;
 import cpdt.common.enums.ProcessArea;
 
 public class Location {
+
     private String locationId;
-
     private String name;
-
     private ProcessArea area;
+
+    public Location(){}
+
+    public Location(String locationId, String name, ProcessArea area){
+        this.locationId = locationId;
+        this.name = name;
+        this.area = area;
+    }
 }
