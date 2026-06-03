72a501402beb70e5ed69260bbf174934fbc1ee5b
LakshmiSanjeev <lakshmi.sanjeev2023@vitstudent.ac.in>
2026-06-03 22:13:22 +0530

    Telemetry and alert DTOs

--- common/src/main/java/cpdt/common/dto/AlertMessage.java
+++ common/src/main/java/cpdt/common/dto/AlertMessage.java
@@ -2,21 +2,10 @@ package cpdt.common.dto;
 
 import cpdt.common.enums.AlertSeverity;
 
-public class AlertMessage {
-
-    private String alertId;
-    private String deviceId;
-    private AlertSeverity severity;
-    private String message;
-    private long timestamp;
-
-    public AlertMessage() {}
-
-    public AlertMessage(String alertId, String deviceId, AlertSeverity severity, String message, long timestamp){
-        this.alertId = alertId;
-        this.deviceId = deviceId;
-        this.severity = severity;
-        this.message = message;
-        this.timestamp = timestamp;
-    }
-}
+public record AlertMessage(
+        String alertId,
+        String deviceId,
+        AlertSeverity severity,
+        String message,
+        long timestamp
+) {}
--- common/src/main/java/cpdt/common/dto/TelemetryPacket.java
+++ common/src/main/java/cpdt/common/dto/TelemetryPacket.java
@@ -1,23 +1,15 @@
 package cpdt.common.dto;
 
+import cpdt.common.enums.DeviceStatus;
 import cpdt.common.enums.DeviceType;
 import cpdt.common.enums.MeasurementType;
 
 import java.util.Map;
 
-public class TelemetryPacket {
-
-    private String deviceId;
-    private DeviceType deviceType;
-    private long timestamp;
-    private Map<MeasurementType, Double> values;
-
-    public TelemetryPacket(){}
-
-    public TelemetryPacket(String deviceId, DeviceType deviceType, long timestamp, Map<MeasurementType, Double> values){
-        this.deviceId = deviceId;
-        this.deviceType = deviceType;
-        this.timestamp = timestamp;
-        this.values = values;
-    }
-}
+public record TelemetryPacket(
+        String deviceId,
+        DeviceType deviceType,
+        long timestamp,
+        Map<MeasurementType, Double> values,
+        DeviceStatus status
+) {}
