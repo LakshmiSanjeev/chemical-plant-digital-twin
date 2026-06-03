9f9095ffff4e02ffd50dd25181097f245ee1a6c6
LakshmiSanjeev <lakshmi.sanjeev2023@vitstudent.ac.in>
2026-06-03 22:14:32 +0530

    Device and location

--- common/src/main/java/cpdt/common/models/Device.java
+++ common/src/main/java/cpdt/common/models/Device.java
@@ -2,16 +2,26 @@ package cpdt.common.models;
 
 import cpdt.common.enums.DeviceStatus;
 import cpdt.common.enums.DeviceType;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
 public abstract class Device {
 
-    private String deviceId;
-    private String name;
-    private DeviceType type;
-    private Location location;
-    private DeviceStatus status;
+    @EqualsAndHashCode.Include
+    private final String deviceId;
+    private final String name;
+    private final DeviceType type;
+    private final Location location;
 
-    protected Device(){}
+    @Setter
+    private DeviceStatus status;
+    @Setter
+    private long lastUpdated;
 
     protected Device(String deviceId, String name, DeviceType type, Location location, DeviceStatus status){
         this.deviceId = deviceId;
@@ -19,5 +29,6 @@ public abstract class Device {
         this.type = type;
         this.location = location;
         this.status = status;
+        this.lastUpdated = System.currentTimeMillis();
     }
 }
--- common/src/main/java/cpdt/common/models/Location.java
+++ common/src/main/java/cpdt/common/models/Location.java
@@ -2,17 +2,7 @@ package cpdt.common.models;
 
 import cpdt.common.enums.ProcessArea;
 
-public class Location {
-
-    private String locationId;
-    private String name;
-    private ProcessArea area;
-
-    public Location(){}
-
-    public Location(String locationId, String name, ProcessArea area){
-        this.locationId = locationId;
-        this.name = name;
-        this.area = area;
-    }
-}
+public record Location(
+        String locationId,
+        String name,
+        ProcessArea area) { }
