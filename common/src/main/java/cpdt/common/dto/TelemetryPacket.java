ed0ac11e229a0d3c444208a0bdc9a7fb62b7fb5c
LakshmiSanjeev <lakshmi.sanjeev2023@vitstudent.ac.in>
2026-06-21 18:50:24 +0530

    Updated telemetry packet with location data

--- common/src/main/java/cpdt/common/dto/TelemetryPacket.java
+++ common/src/main/java/cpdt/common/dto/TelemetryPacket.java
@@ -1,13 +1,15 @@
 package cpdt.common.dto;
 
-import cpdt.common.enums.DeviceStatus;
-import cpdt.common.enums.DeviceType;
-import cpdt.common.enums.MeasurementType;
+import cpdt.common.enums.*;
 
 public record TelemetryPacket(
         String deviceId,
+        String deviceName,
         DeviceType deviceType,
         DeviceStatus status,
+        String locationId,
+        String locationName,
+        ProcessArea processArea,
         long timestamp,
         MeasurementType measurementType,
         double value
