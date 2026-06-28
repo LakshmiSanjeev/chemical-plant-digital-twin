3f9c42b9be8858b2dced308ab1c7109afc121e73
LakshmiSanjeev <lakshmi.sanjeev2023@vitstudent.ac.in>
2026-06-28 23:46:31 +0530

    Resources, enums

---
+++ common/src/main/java/cpdt/common/enums/AlarmState.java
@@ -0,0 +1,14 @@
+package cpdt.common.enums;
+
+public enum AlarmState {
+
+    NEW_ALARM,
+
+    ESCALATED,
+
+    DEESCALATED,
+
+    CLEARED,
+
+    NO_CHANGE
+}
--- common/src/main/java/cpdt/common/enums/AlertSeverity.java
+++ common/src/main/java/cpdt/common/enums/AlertSeverity.java
@@ -2,6 +2,8 @@ package cpdt.common.enums;
 
 public enum AlertSeverity {
     INFO,
-    WARNING,
-    CRITICAL
+    WARNING_LOW,
+    WARNING_HIGH,
+    CRITICAL_HIGH,
+    CRITICAL_LOW
 }
